aa1e11e5b319a5b993c61e302604c3e8955d5b64
Tomasz Borusiewicz <tomasz.borusiewicz147@gmail.com>
2019-05-12 16:29:48 +0200

    Update test_diet.py

--- test_diet.py
+++ test_diet.py
@@ -1,4 +1,13 @@
-import diet_algoritm.diet as diet
+import diet_algoritm.diet
+import csv
+algoritms = diet_algoritm.diet.Algorithms("products.csv", 1000)
+
+
+# def test_get_sample_product():
+sample_product = algoritms.get_product_from_csv(0)
+print(sample_product)
+all_ids = algoritms.get_random_product_from_csv()
+print(all_ids)
 
 # dieet = diet.Diet(100, 180, "male")
 #
@@ -8,9 +17,19 @@ import diet_algoritm.diet as diet
 #     print(test)
 
 
-waga = input("Podaj swoją wagę: ")
-wzrost = input("Podaj swój wzrost: ")
-plec = input("podaj swoją płeć: ")
+# waga = input("Podaj swoją wagę: ")
+# wzrost = input("Podaj swój wzrost: ")
+# plec = input("podaj swoją płeć: ")
+#
+# dieta = diet.Diet(int(waga), int(wzrost), plec)
+# print(dieta.print_monthly_mekroelements_demand())
 
-dieta = diet.Diet(int(waga), int(wzrost), plec)
-print(dieta.print_monthly_mekroelements_demand())
+# def get_product_from_csv(id):
+#     with open("products.csv", encoding="utf8") as file:
+#         products = csv.reader(file, delimiter=",")
+#         for row in products:
+#             print(row)
+#
+#
+# dupa = get_product_from_csv(1)
+# print(dupa)
