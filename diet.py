a5a78afec987aee2fe9d8dd585e08ba4642ce052
Tomasz <tomasz.borusiewicz147@gmail.com>
2019-05-14 22:02:13 +0200

    update

--- diet.py
+++ diet.py
@@ -9,6 +9,7 @@ class Diet(object):
         self.height = height
         self.sex = sex
         self.price = price
+        self.max_searching_counter = 10000000
 
     def kilocalorie_demand_for_month(self):
         return int((33 * self.weight) * 30)
@@ -59,30 +60,80 @@ class Diet(object):
         return self.get_product_from_csv(random_id)
 
     def check_if_actual_price_not_exceed_given_price(self, actual_price):
-        if actual_price < self.price:
-            return True
+        if self.max_searching_counter > 0:
+            self.max_searching_counter -= 1
+            if actual_price < self.price:
+                return True
+        else:
+            print("program nie potrafi znaleść diety dla podanego zakresu cenowego")
+            exit(0)
 
-    def get_macro_which_is_more_than_given_percent_if_exist(self, carbohydrates, protein, fat, percent):
-        more_than = []
-        less_than = []
-        all_demands = self.makroelements_demand_for_month()
-        if carbohydrates > all_demands["weglowodany"] * percent:
-            more_than.append("weglowodany")
-        else:
-            less_than.append("weglowodany")
-        if protein > all_demands["bialko"] * percent:
-            more_than.append("bialko")
-        else:
-            less_than.append("bialko")
-        if fat > all_demands["tluszcz"] * percent:
-            more_than.append("tluszcz")
-        else:
-            less_than.append("tluszcz")
-        print(more_than, less_than)
-        if more_than and less_than:
-            return more_than, less_than
-        else:
-            return False
+    def get_products_from_csv_where_makroelement_has_value_less_than(self, makroelement_name, value):
+        listed_products = []
+        flag = 0
+        if makroelement_name == "weglowodany":
+            flag = 3
+        elif makroelement_name == "bialko":
+            flag = 1
+        elif makroelement_name == "tluszcz":
+            flag = 2
+        with open(self.file_with_products, encoding="utf8") as file:
+            products = csv.reader(file, delimiter=",")
+            for row in products:
+                if float(row[flag]) < value:
+                    listed_products.append(row)
+        return listed_products
+
+    def get_product_from_csv_where_catbo_is_less_than(self, less_than_value):
+        carbon_products = self.get_products_from_csv_where_makroelement_has_value_less_than("weglowodany",
+                                                                                            less_than_value)
+        random_number = random.randint(0, len(carbon_products))
+        random_product = carbon_products[random_number]
+        return random_product
+
+    def get_product_from_csv_where_protein_is_less_than(self, less_than_value):
+        protein_products = self.get_products_from_csv_where_makroelement_has_value_less_than("bialko",
+                                                                                             less_than_value)
+        random_number = random.randint(0, len(protein_products))
+        random_product = protein_products[random_number]
+        return random_product
+
+    def get_product_from_csv_where_fat_is_less_than(self, less_than_value):
+        fat_products = self.get_products_from_csv_where_makroelement_has_value_less_than("tluszcz",
+                                                                                         less_than_value)
+        random_number = random.randint(0, len(fat_products) - 1)
+        random_product = fat_products[random_number]
+        return random_product
+
+    def get_product_from_csv_where_catbo_and_protein_is_less_than(self, less_than_value):
+        carbon_product = self.get_products_from_csv_where_makroelement_has_value_less_than("weglowodany",
+                                                                                           less_than_value)
+        protein_product = self.get_products_from_csv_where_makroelement_has_value_less_than("bialko",
+                                                                                            less_than_value)
+        compared_list = [i for i in carbon_product if i in protein_product]
+        random_number = random.randint(0, len(compared_list))
+        random_product = compared_list[random_number]
+        return random_product
+
+    def get_product_from_csv_where_carbo_and_fat_is_less_than(self, less_than_value):
+        carbon_product = self.get_products_from_csv_where_makroelement_has_value_less_than("weglowodany",
+                                                                                           less_than_value)
+        fat_product = self.get_products_from_csv_where_makroelement_has_value_less_than("tluszcz",
+                                                                                        less_than_value)
+        compared_list = [i for i in carbon_product if i in fat_product]
+        random_number = random.randint(0, len(compared_list) - 1)
+        random_product = compared_list[random_number]
+        return random_product
+
+    def get_product_from_csv_where_protein_and_fat_is_less_than(self, less_than_value):
+        fat_product = self.get_products_from_csv_where_makroelement_has_value_less_than("tluszcz",
+                                                                                        less_than_value)
+        protein_product = self.get_products_from_csv_where_makroelement_has_value_less_than("bialko",
+                                                                                            less_than_value)
+        compared_list = [i for i in fat_product if i in protein_product]
+        random_number = random.randint(0, len(compared_list) - 1)
+        random_product = compared_list[random_number]
+        return random_product
 
     def create_random_diet(self):
         products_list = []
@@ -91,67 +142,123 @@ class Diet(object):
         actual_protein = 0
         actual_fat = 0
         all_demands = self.makroelements_demand_for_month()
-        while (int(actual_carbohydrates) < int(all_demands["weglowodany"] * 0.9)
-                and int(actual_protein) < int(all_demands["bialko"] * 0.9)
-                and int(actual_fat) < int(all_demands["tluszcz"] * 0.9)):
-            try:
-                random_product = self.get_random_product_from_csv()
-                actual_carbohydrates += float(random_product[3])
-                actual_protein += float(random_product[1])
-                actual_fat += float(random_product[2])
-                actual_price += float(random_product[4])
-                products_list.append(random_product[0])
-            except TypeError:
-                continue
-            if self.check_if_actual_price_not_exceed_given_price(actual_price):
-                continue
-            else:
-                self.create_random_diet()
-        # check_macros = self.get_macro_which_is_more_than_given_percent_if_exist(actual_carbohydrates,
-        #                                                                         actual_protein,
-        #                                                                         actual_fat,
-        #                                                                         0.9)
-        # print(check_macros)
-        # if check_macros:
-        #     print(actual_carbohydrates)
-        #     while (int(actual_carbohydrates) > int(all_demands[check_macros[0][0]] * 0.9)
-        #            and int(actual_protein) < int(all_demands[check_macros[1][0]] * 0.9)
-        #            and int(actual_fat) < int(all_demands[check_macros[1][1]] * 0.9)):
-        #         try:
-        #             random_product = self.get_random_product_from_csv()
-        #             actual_carbohydrates += float(random_product[3])
-        #             actual_protein += float(random_product[1])
-        #             actual_fat += float(random_product[2])
-        #             actual_price += float(random_product[4])
-        #             products_list.append(random_product[0])
-        #         except TypeError:
-        #             continue
-        #         if self.check_if_actual_price_not_exceed_given_price(actual_price):
-        #             continue
-        #         else:
-        #             self.create_random_diet()
-        # check_macros = self.get_macro_which_is_more_than_given_percent_if_exist(actual_carbohydrates,
-        #                                                                         actual_protein,
-        #                                                                         actual_fat,
-        #                                                                         0.9)
-        # if check_macros:
-        #     while (int(actual_carbohydrates) > int(all_demands[check_macros[0][0]] * 0.9)
-        #            and int(actual_protein) > int(all_demands[check_macros[0][1]] * 0.9)
-        #            and int(actual_fat) < int(all_demands[check_macros[1][0]] * 0.9)):
-        #         try:
-        #             random_product = self.get_random_product_from_csv()
-        #             actual_carbohydrates += float(random_product[3])
-        #             actual_protein += float(random_product[1])
-        #             actual_fat += float(random_product[2])
-        #             actual_price += float(random_product[4])
-        #             products_list.append(random_product[0])
-        #         except TypeError:
-        #             continue
-        #         if self.check_if_actual_price_not_exceed_given_price(actual_price):
-        #             continue
-        #         else:
-        #             self.create_random_diet()
-        print(actual_carbohydrates, actual_protein, actual_fat, actual_price)
-        return products_list
+        while (int(actual_carbohydrates) < int(all_demands["weglowodany"] * 0.99)
+               or int(actual_protein) < int(all_demands["bialko"] * 0.99)
+               or int(actual_fat) < int(all_demands["tluszcz"] * 0.99)):
+
+            if int(actual_carbohydrates) < int(all_demands["weglowodany"] * 0.99)\
+                    and int(actual_protein) < int(all_demands["bialko"] * 0.99)\
+                    and int(actual_fat) < int(all_demands["tluszcz"] * 0.99):
+                try:
+                    random_product = self.get_random_product_from_csv()
+                    actual_carbohydrates += float(random_product[3])
+                    actual_protein += float(random_product[1])
+                    actual_fat += float(random_product[2])
+                    actual_price += float(random_product[4])
+                    products_list.append(random_product[0])
+                except TypeError:
+                    continue
+                if self.check_if_actual_price_not_exceed_given_price(actual_price):
+                    continue
+                else:
+                    self.create_random_diet()
+            elif (int(actual_carbohydrates) >= int(all_demands["weglowodany"] * 0.99)
+                    and int(actual_protein) < int(all_demands["bialko"] * 0.99)
+                    and int(actual_fat) < int(all_demands["tluszcz"] * 0.99)):
+                try:
+                    random_product = self.get_product_from_csv_where_catbo_is_less_than(0.5)
+                    actual_carbohydrates += float(random_product[3])
+                    actual_protein += float(random_product[1])
+                    actual_fat += float(random_product[2])
+                    actual_price += float(random_product[4])
+                    products_list.append(random_product[0])
+                except TypeError:
+                    continue
+                if self.check_if_actual_price_not_exceed_given_price(actual_price):
+                    continue
+                else:
+                    self.create_random_diet()
+            elif (int(actual_carbohydrates) < int(all_demands["weglowodany"] * 0.99)
+                    and int(actual_protein) >= int(all_demands["bialko"] * 0.99)
+                    and int(actual_fat) < int(all_demands["tluszcz"] * 0.99)):
+                try:
+                    random_product = self.get_product_from_csv_where_protein_is_less_than(0.5)
+                    actual_carbohydrates += float(random_product[3])
+                    actual_protein += float(random_product[1])
+                    actual_fat += float(random_product[2])
+                    actual_price += float(random_product[4])
+                    products_list.append(random_product[0])
+                except TypeError:
+                    continue
+                if self.check_if_actual_price_not_exceed_given_price(actual_price):
+                    continue
+                else:
+                    self.create_random_diet()
+            elif (int(actual_carbohydrates) < int(all_demands["weglowodany"] * 0.99)
+                    and int(actual_protein) < int(all_demands["bialko"] * 0.99)
+                    and int(actual_fat) >= int(all_demands["tluszcz"] * 0.99)):
+                try:
+                    random_product = self.get_product_from_csv_where_fat_is_less_than(0.5)
+                    actual_carbohydrates += float(random_product[3])
+                    actual_protein += float(random_product[1])
+                    actual_fat += float(random_product[2])
+                    actual_price += float(random_product[4])
+                    products_list.append(random_product[0])
+                except TypeError:
+                    continue
+                if self.check_if_actual_price_not_exceed_given_price(actual_price):
+                    continue
+                else:
+                    self.create_random_diet()
+            elif (int(actual_carbohydrates) >= int(all_demands["weglowodany"] * 0.99)
+                    and int(actual_protein) >= int(all_demands["bialko"] * 0.99)
+                    and int(actual_fat) < int(all_demands["tluszcz"] * 0.99)):
+                try:
+                    random_product = self.get_product_from_csv_where_catbo_and_protein_is_less_than(0.5)
+                    actual_carbohydrates += float(random_product[3])
+                    actual_protein += float(random_product[1])
+                    actual_fat += float(random_product[2])
+                    actual_price += float(random_product[4])
+                    products_list.append(random_product[0])
+                except TypeError:
+                    continue
+                if self.check_if_actual_price_not_exceed_given_price(actual_price):
+                    continue
+                else:
+                    self.create_random_diet()
+            elif (int(actual_carbohydrates) >= int(all_demands["weglowodany"] * 0.99)
+                    and int(actual_protein) < int(all_demands["bialko"] * 0.99)
+                    and int(actual_fat) >= int(all_demands["tluszcz"] * 0.99)):
+                try:
+                    random_product = self.get_product_from_csv_where_carbo_and_fat_is_less_than(0.5)
+                    actual_carbohydrates += float(random_product[3])
+                    actual_protein += float(random_product[1])
+                    actual_fat += float(random_product[2])
+                    actual_price += float(random_product[4])
+                    products_list.append(random_product[0])
+                except TypeError:
+                    continue
+                if self.check_if_actual_price_not_exceed_given_price(actual_price):
+                    continue
+                else:
+                    self.create_random_diet()
+            elif (int(actual_carbohydrates) < int(all_demands["weglowodany"] * 0.99)
+                    and int(actual_protein) >= int(all_demands["bialko"] * 0.99)
+                    and int(actual_fat) >= int(all_demands["tluszcz"] * 0.99)):
+                try:
+                    random_product = self.get_product_from_csv_where_protein_and_fat_is_less_than(0.5)
+                    actual_carbohydrates += float(random_product[3])
+                    actual_protein += float(random_product[1])
+                    actual_fat += float(random_product[2])
+                    actual_price += float(random_product[4])
+                    products_list.append(random_product[0])
+                except TypeError:
+                    continue
+                if self.check_if_actual_price_not_exceed_given_price(actual_price):
+                    continue
+                else:
+                    self.create_random_diet()
+        information = [int(actual_carbohydrates), int(actual_protein), int(actual_fat * 0.95), int(actual_price)]
+        return products_list, information
 
 
--- make_diet.py
+++ make_diet.py
@@ -1,12 +1,14 @@
 from diet import Diet
 import time
 import collections
+import  datetime
 while True:
     print("========================================================================================")
     print("================================== Kreator Diety =======================================")
     print("1. Stwórz dietę (algorytm losowy)")
     print("2. Wyjdz z kreatora")
-    choice = input("Co chcesz zrobić?(wybierz 1/2): ")
+    # choice = input("Wybierz 1/2: ")
+    choice = "1"
     if choice == "2":
         exit(0)
     elif choice == "1":
@@ -14,20 +16,29 @@ while True:
         # height = input("Podaj swoj wzrost[cm]: ")
         # sex = input("Podaj swoją płeć(wpisz mezczyzna/kobieta): ")
         # diet_price = input("Wpisz kwotę którą przeznaczysz na miesięczną dietę[zł]: ")
-        weight = 80
+        weight = 50
         height = 180
         sex = "mezczyzna"
-        diet_price = 2000
+        diet_price = 1800
         diet = Diet(weight=weight,
                     height=height,
                     sex=sex,
                     csv_file_with_products="products.csv",
                     price=diet_price
                     )
-        print(diet.print_monthly_mekroelements_demand())
+        diet.print_monthly_mekroelements_demand()
+        time_before = time.time()
         sample_diet = diet.create_random_diet()
-        dupa = collections.Counter(sample_diet)
-        print(type(dupa))
+        time_after = time.time()
+        dupa = collections.Counter(sample_diet[0])
+        print("==================================================================================")
+        print("Makroelementy na miesiąc w znalezionej diecie:\nWęglowodany: {} gram\nBiałko: {} gram"
+              "\nTłuszcz: {} gram\nCena diety {} złotych".format(sample_diet[1][0], sample_diet[1][1],
+                                                               sample_diet[1][2], sample_diet[1][3]))
+        for i in dupa:
+            print("{}  {} gram".format(i, dupa[i]*100))
+
+        print("czas szukania: {} sekund".format(time_after - time_before))
 
     else:
         print("wpisano nieprawidłową wartość, spróbuj ponownie")
